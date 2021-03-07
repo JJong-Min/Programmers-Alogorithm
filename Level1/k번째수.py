adffb62911fbedb7ff57e607957ea382a017ab6c
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-03-07 20:22:07 +0900

    Correct: K번째 수
    Programmers-코딩테스트 연습문제
    정렬

---
+++ Level1/k번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i  = command[0]
+        j = command[1]
+        k = command[2]
+        answer.append(sorted(list(array[i-1:j]))[k-1])
+        
+    return answer
