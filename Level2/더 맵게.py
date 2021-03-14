7fa9f2c3c52c3d9e551d913780fca8203475fb43
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-03-14 23:18:06 +0900

    Correct: 더 맵게
    Programmers - 코딩테스트 연습
    힙(heap)

---
+++ Level2/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+def solution(scoville, k):
+    heap = []
+    for num in scoville:
+        heapq.heappush(heap, num)
+    answer = 0
+    while heap[0] <= k:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
+        except IndexError:
+            return -1
+        answer += 1
+     
+    return answer
