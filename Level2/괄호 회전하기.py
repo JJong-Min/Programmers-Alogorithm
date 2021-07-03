4a11a8930f1f3761ed83e4b8c565f0d2a9f755c5
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-03 12:12:17 +0900

    Correct: 괄호회전하기
    Programmers-코딩테스트연습
    BFS

---
+++ Level2/괄호 회전하기.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(s):
+    answer = 0
+    dic = {'{':'}', '[':']', '(':')'}
+           
+    for i in range(len(s)):
+        right = deque(list(s[i:] + s[:i]))
+        left = []
+        while right:
+            temp = right.popleft()
+            if not left:
+                left.append(temp)
+            else:
+                if left[-1] in ['}', ')', ']']:
+                    break
+                    
+                if dic[left[-1]] == temp:
+                    left.pop()
+                else:
+                    left.append(temp)
+        
+        if not left:
+            answer += 1
+            
+    return answer
